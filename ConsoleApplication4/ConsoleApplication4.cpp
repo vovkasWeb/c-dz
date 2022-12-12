37a9326e493c745a4f8471f71e9b10ff503e5ac6
vovkaWeb <vova77557@gmail.com>
2022-12-12 21:29:46 +0200

    first commit

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -59,10 +59,25 @@ int* deleteElement(int* arr, int length, int index) {
 	return newArray;
 }
 
+int* DelNeg(int* Arr, int n, int& k)
+{
+	int i, j, * r;
+	k = 0;
+	for (i = 0; i < n; i++) if (Arr[i] >= 0) k++;
+	r = new int[k];
+	j = 0;
+	for (i = 0; i < n; i++) if (Arr[i] >= 0) r[j++] = Arr[i];
+	delete[] Arr;
+	return r;
+}
+
 int main()
 {
 	srand(time(0));
-	int* array = new int[n];
+
+	//1
+
+	/*int* array = new int[n];
 
 	initArr(array, n);
 	printArr(array, n);
@@ -72,7 +87,30 @@ int main()
 	printArr(array, n);
 	array = deleteElement(array, n, 4);
 	printArr(array, n);
-	deleteArr(array);
+	deleteArr(array);*/
 	
+	//2
+	int* A = new int[10];
+	int i, n, * r;
 
+	A[0] = 1;
+	A[1] = -1;
+	A[2] = 16;
+	A[3] = -81;
+	A[4] = 0;
+	A[5] = 0;
+	A[6] = 12;
+	A[7] = 121;
+	A[8] = -11;
+	A[9] = 11;
+
+	r = DelNeg(A, 10, n);
+
+	for (i = 0; i < n; i++) cout << r[i] << " ";
+
+	cout << endl;
+
+	delete[] r;
+
+	
 }
